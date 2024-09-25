64e027c8d853d143c0dc484881c62d0febdd71b9
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-09-25 11:19:21 +0300

    feat: add header with responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import '../scss/style.scss';
 import { isWebp } from './files/helpers.js';
+import { initMenu } from './files/burger.js';
 
 window.addEventListener('load', () => {
   isWebp();
+  initMenu();
 });
