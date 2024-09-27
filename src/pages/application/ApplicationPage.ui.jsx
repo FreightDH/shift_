fd1876760c334c02eab2e663e2e3d15b4288489a
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-09-27 17:08:30 +0300

    feat: add responsive to application page

--- src/pages/application/ApplicationPage.ui.jsx
+++ src/pages/application/ApplicationPage.ui.jsx
@@ -13,18 +13,16 @@ export const ApplicationPage = () => {
         <section className={cl.application}>
           <div className="application__container">
             <div className={cl.application__body}>
-              <div className={cl.application__content}>
-                <div className={cn(cl.application__square, {}, ['square', 'bg__grey'])}></div>
-                <div className={cl.application__titles}>
-                  <h1 className={cl.application__title}>Подача заявления</h1>
-                  <h2 className={cl.application__subtitle}>
-                    Оставьте Ваш телеграм, и мы сами Вам напишем и про все расскажем
-                  </h2>
-                </div>
-                <div className={cl.application__controls}>
-                  <CustomInput placeholder="Ваш телеграм" value="" onChange={() => {}} />
-                  <CustomButton></CustomButton>
-                </div>
+              <div className={cn(cl.application__square, {}, ['square', 'bg__grey'])}></div>
+              <div className={cl.application__titles}>
+                <h1 className={cl.application__title}>Подача заявления</h1>
+                <h2 className={cl.application__subtitle}>
+                  Оставьте Ваш телеграм, и мы сами Вам напишем и про все расскажем
+                </h2>
+              </div>
+              <div className={cl.application__controls}>
+                <CustomInput placeholder="Ваш телеграм" value="" onChange={() => {}} />
+                <CustomButton />
               </div>
             </div>
           </div>
